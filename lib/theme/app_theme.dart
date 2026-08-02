1ea81b0cfc3dfedeb398547ca85a40331cd15b6a
Kidanedg <gkidanedesta@gmail.com>
2026-08-02 12:57:39 +0300

    Modify light theme with new styles and colors
    Updated scaffold background color and added themes for input decoration and elevated buttons.

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -5,10 +5,27 @@ class AppTheme {
     return ThemeData(
       useMaterial3: true,
       colorSchemeSeed: Colors.indigo,
-      scaffoldBackgroundColor: Colors.white,
+      scaffoldBackgroundColor: Colors.grey.shade100,
+
       appBarTheme: const AppBarTheme(
         centerTitle: true,
         elevation: 0,
+      ),
+
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(12),
+          ),
+        ),
+      ),
+
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size.fromHeight(50),
+        ),
       ),
     );
   }
