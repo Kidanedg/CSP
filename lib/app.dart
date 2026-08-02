c3e824dfb9a0c482b3bf15825313ff0464c3b5a1
Kidanedg <gkidanedesta@gmail.com>
2026-08-02 12:56:49 +0300

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,19 @@
-MaterialApp(
-  debugShowCheckedModeBanner: false,
-  title: "CSP",
-  theme: AppTheme.lightTheme,
-  initialRoute: AppRoutes.splash,
-  routes: AppRoutes.routes,
-);
+import 'package:flutter/material.dart';
+
+import 'routes/app_routes.dart';
+import 'theme/app_theme.dart';
+
+class CSPApp extends StatelessWidget {
+  const CSPApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CSP',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      initialRoute: AppRoutes.splash,
+      routes: AppRoutes.routes,
+    );
+  }
+}
