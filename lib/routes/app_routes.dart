75c9480e30f8d4ceba14fb34c98bf40b32ede1e3
Kidanedg <gkidanedesta@gmail.com>
2026-08-02 16:35:41 +0300

    Add forgot password route to app routes

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../screens/splash/splash_screen.dart';
 import '../screens/login/login_screen.dart';
 import '../screens/register/register_screen.dart';
+import '../screens/forgot_password/forgot_password_screen.dart';
 import '../screens/home/home_screen.dart';
 import '../screens/chat/chat_screen.dart';
 import '../screens/profile/profile_screen.dart';
@@ -12,6 +13,7 @@ class AppRoutes {
   static const splash = "/";
   static const login = "/login";
   static const register = "/register";
+  static const forgotPassword = "/forgot-password";
   static const home = "/home";
   static const chat = "/chat";
   static const profile = "/profile";
@@ -21,6 +23,7 @@ class AppRoutes {
         splash: (_) => const SplashScreen(),
         login: (_) => const LoginScreen(),
         register: (_) => const RegisterScreen(),
+        forgotPassword: (_) => const ForgotPasswordScreen(),
         home: (_) => const HomeScreen(),
         chat: (_) => const ChatScreen(),
         profile: (_) => const ProfileScreen(),
