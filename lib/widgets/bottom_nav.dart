a56ceee761cfdcf259251c8adcd74650a83d6f01
Kidanedg <gkidanedesta@gmail.com>
2026-08-02 12:39:39 +0300

    Update bottom_nav.dart

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -1,1 +1,42 @@
+import 'package:flutter/material.dart';
 
+class BottomNavigation extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const BottomNavigation({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      selectedIndex: currentIndex,
+      onDestinationSelected: onTap,
+      destinations: const [
+        NavigationDestination(
+          icon: Icon(Icons.home_outlined),
+          selectedIcon: Icon(Icons.home),
+          label: "Home",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.chat_outlined),
+          selectedIcon: Icon(Icons.chat),
+          label: "Chats",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.person_outline),
+          selectedIcon: Icon(Icons.person),
+          label: "Profile",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.settings_outlined),
+          selectedIcon: Icon(Icons.settings),
+          label: "Settings",
+        ),
+      ],
+    );
+  }
+}
