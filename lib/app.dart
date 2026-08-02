7ae5ef53338f031f1fbaa69bd3fbf105a7b759f1
Kidanedg <gkidanedesta@gmail.com>
2026-08-02 12:53:44 +0300

    Refactor CSPApp to remove unnecessary class structure

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,7 @@
-import 'package:flutter/material.dart';
-
-import 'theme/app_theme.dart';
-import 'routes/app_routes.dart';
-
-class CSPApp extends StatelessWidget {
-  const CSPApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'CSP',
-      theme: AppTheme.lightTheme,
-      initialRoute: AppRoutes.splash,
-      routes: AppRoutes.routes,
-    );
-  }
-}
+MaterialApp(
+  debugShowCheckedModeBanner: false,
+  title: "CSP",
+  theme: AppTheme.lightTheme,
+  initialRoute: AppRoutes.splash,
+  routes: AppRoutes.routes,
+);
