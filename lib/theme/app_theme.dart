b940cf0fe1eeeae3870a4d0bbb372e65332961c0
Kidanedg <gkidanedesta@gmail.com>
2026-08-02 12:32:03 +0300

    Implement light theme in AppTheme class
    Add light theme configuration for the app.

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: Colors.indigo,
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: const AppBarTheme(
+        centerTitle: true,
+        elevation: 0,
+      ),
+    );
+  }
+}
