50c3e0a790c1ae7d18b5615a5d8b6e92cb0c0338
Kidanedg <gkidanedesta@gmail.com>
2026-08-02 16:57:25 +0300

    Update auth_user.dart

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -1,1 +1,27 @@
+class AuthUser {
+  final String id;
+  final String name;
+  final String email;
+  final bool emailVerified;
 
+  const AuthUser({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.emailVerified = false,
+  });
+
+  AuthUser copyWith({
+    String? id,
+    String? name,
+    String? email,
+    bool? emailVerified,
+  }) {
+    return AuthUser(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      emailVerified: emailVerified ?? this.emailVerified,
+    );
+  }
+}
