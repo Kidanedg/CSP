f2e177a75c081b442ddfecadb1040b5186bfd87f
Kidanedg <gkidanedesta@gmail.com>
2026-08-02 17:04:22 +0300

    Add PreferenceKeys class for storage constants

---
+++ lib/services/storage/preference_keys.dart
@@ -0,0 +1,13 @@
+class PreferenceKeys {
+  PreferenceKeys._();
+
+  static const String rememberMe = 'remember_me';
+  static const String isLoggedIn = 'is_logged_in';
+  static const String authToken = 'auth_token';
+  static const String userId = 'user_id';
+  static const String email = 'email';
+  static const String displayName = 'display_name';
+  static const String themeMode = 'theme_mode';
+  static const String language = 'language';
+  static const String firstLaunch = 'first_launch';
+}
