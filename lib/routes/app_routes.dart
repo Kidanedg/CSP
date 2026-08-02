ea222d32493f8163e9b74cc551fb96d8bf362a66
Kidanedg <gkidanedesta@gmail.com>
2026-08-02 12:32:38 +0300

    Add app routes for navigation in Flutter app

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+import '../screens/splash/splash_screen.dart';
+import '../screens/login/login_screen.dart';
+import '../screens/register/register_screen.dart';
+import '../screens/home/home_screen.dart';
+import '../screens/chat/chat_screen.dart';
+import '../screens/profile/profile_screen.dart';
+import '../screens/settings/settings_screen.dart';
+
+class AppRoutes {
+  static const splash = "/";
+  static const login = "/login";
+  static const register = "/register";
+  static const home = "/home";
+  static const chat = "/chat";
+  static const profile = "/profile";
+  static const settings = "/settings";
+
+  static Map<String, WidgetBuilder> get routes => {
+        splash: (_) => const SplashScreen(),
+        login: (_) => const LoginScreen(),
+        register: (_) => const RegisterScreen(),
+        home: (_) => const HomeScreen(),
+        chat: (_) => const ChatScreen(),
+        profile: (_) => const ProfileScreen(),
+        settings: (_) => const SettingsScreen(),
+      };
+}
