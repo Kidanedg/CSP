fa6c1548c3b4ef569721465307d84c7f99ff83bc
Kidanedg <gkidanedesta@gmail.com>
2026-08-02 14:55:21 +0300

    Create AuthTextField widget with password visibility toggle
    Added AuthTextField widget for user authentication input.

--- lib/widgets/auth_text_field.dart
+++ lib/widgets/auth_text_field.dart
@@ -1,1 +1,69 @@
+import 'package:flutter/material.dart';
 
+class AuthTextField extends StatefulWidget {
+  final TextEditingController controller;
+  final String label;
+  final String hint;
+  final IconData icon;
+  final bool isPassword;
+  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+
+  const AuthTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    required this.hint,
+    required this.icon,
+    this.isPassword = false,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+  });
+
+  @override
+  State<AuthTextField> createState() => _AuthTextFieldState();
+}
+
+class _AuthTextFieldState extends State<AuthTextField> {
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final isPassword = widget.isPassword;
+
+    return TextFormField(
+      controller: widget.controller,
+      keyboardType: widget.keyboardType,
+      validator: widget.validator,
+      obscureText: isPassword ? _obscureText : false,
+      decoration: InputDecoration(
+        labelText: widget.label,
+        hintText: widget.hint,
+        prefixIcon: Icon(widget.icon),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        suffixIcon: isPassword
+            ? IconButton(
+                icon: Icon(
+                  _obscureText
+                      ? Icons.visibility_off
+                      : Icons.visibility,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+              )
+            : null,
+      ),
+    );
+  }
+}
