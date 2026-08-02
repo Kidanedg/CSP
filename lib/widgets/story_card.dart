9705b41b29a37253182fbdfeb12f8444d976451e
Kidanedg <gkidanedesta@gmail.com>
2026-08-02 12:39:21 +0300

    Create StoryCard widget for displaying user stories
    Added a StoryCard widget that displays a user avatar and story title.

--- lib/widgets/story_card.dart
+++ lib/widgets/story_card.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+class StoryCard extends StatelessWidget {
+  const StoryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 90,
+      child: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            SizedBox(height: 10),
+            CircleAvatar(
+              radius: 25,
+              child: Icon(Icons.person),
+            ),
+            SizedBox(height: 10),
+            Text("Story"),
+            SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+}
